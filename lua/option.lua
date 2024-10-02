2fe8436135a55e8090ce0c5d52c22cd7fc9f5fe3
Bankaert Benoit <bankaert.benoit@gmail.com>
2024-10-02 13:12:55 +0200

    feat: add lualine

--- lua/option.lua
+++ lua/option.lua
@@ -3,3 +3,4 @@ vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.wrap = false
 vim.opt.title = true
+vim.opt.updatetime = 250
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,7 +4,8 @@ return {
 	priority = 1000,
 	config = function ()
 		require("catppuccin").setup({
-			flavour = "frappe"
+			flavour = "frappe",
+			mason = false
 		})
 		vim.cmd(":colorscheme catppuccin")
 	end
