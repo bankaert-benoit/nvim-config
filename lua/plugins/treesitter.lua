9b48389fdc11fd8b59e0af29ad452700a347fd9e
Bankaert Benoit <bankaert.benoit@gmail.com>
2024-11-05 15:02:38 +0100

    feat: update treesitter conf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,12 @@
+local ENSURE_INSTALLED = { "lua", "html", "css", "typescript", "javascript", "yaml", "svelte", "jsdoc" }
+
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = { "lua", "html", "css", "typescript", "javascript", "yaml", "svelte" },
+			ensure_installed = ENSURE_INSTALLED,
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
