433412565fb819f41195ca82a9ba02308f982fb8
Bankaert Benoit <bankaert.benoit@gmail.com>
2025-03-18 12:06:21 +0100

    feat: main config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,8 +6,9 @@ local lspservers = {
   "html",
   "lua_ls",
   "ts_ls",
-	"svelte",
-	"tailwindcss"
+  "jdtls",
+  "clangd",
+	"rust_analyzer"
 }
 
 local function lsphandlers(lspconfig)
@@ -18,18 +19,12 @@ local function lsphandlers(lspconfig)
     capabilities = capabilities,
   }
 
-  local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-  end
-
   return {
     function(servername)
       lspconfig[servername].setup(opts)
     end,
     ["lua_ls"] = function()
-      lspconfig.lua_ls.setup {
+      lspconfig.lua_ls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
         settings = {
@@ -39,15 +34,50 @@ local function lsphandlers(lspconfig)
             },
           }
         }
-      }
+      })
     end,
-		["angularls"] = function()
-			lspconfig.angularls.setup {
-				on_attach = on_attach,
-				capabilities = capabilities,
-				filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
-			}
-		end,
+    ["angularls"] = function()
+      local initial_root_dir = vim.fn.getcwd()
+      --- Create the run server command
+      --- @param root_dir string The current root dir
+      --- @return table The path params
+      local function create_cmd(root_dir)
+        -- register the local probe location
+        local ts_probe_location = vim.fs.joinpath(root_dir, "node_modules")
+        local ng_probe_location = ts_probe_location
+        -- check if a local language-service server is installed on the current project.
+        local lsp_package_path = vim.fs.joinpath(ts_probe_location, "@angular", "language-service")
+        local local_lsp_is_installed = vim.fn.isdirectory(lsp_package_path) == 1
+        if not local_lsp_is_installed then
+          -- use the global language server
+          local ok, mason_registry = pcall(require, 'mason-registry')
+          if not ok then
+            vim.notify 'mason-registry could not be loaded'
+            return {}
+          end
+          local angularls_path = vim.fs.joinpath(mason_registry.get_package("angular-language-server"):get_install_path(), "node_modules");
+          ts_probe_location = angularls_path
+          ng_probe_location = vim.fs.joinpath(angularls_path, "@angular", "language-server")
+        end
+        return { "ngserver", "--stdio", "--tsProbeLocations", ts_probe_location, "--ngProbeLocations", ng_probe_location }
+      end
+
+      lspconfig.angularls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        cmd = create_cmd(initial_root_dir),
+        on_new_config = function(new_config, new_root_dir)
+          new_config.cmd = create_cmd(new_root_dir)
+        end,
+        filetypes = {
+          "typescript",
+          "html",
+          "typescriptreact",
+          "typescript.tsx",
+          "htmlangular",
+        },
+      })
+    end
   }
 end
 
